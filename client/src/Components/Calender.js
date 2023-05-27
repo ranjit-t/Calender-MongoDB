33014f4fbfbf1b74a3f2eb3174a23dc7fd4ff2f1
Ranjit Thota <amailtoranjith@gmail.com>
2023-05-27 23:52:33 +0200

    two slots in single cell logic

--- client/src/Components/Calender.js
+++ client/src/Components/Calender.js
@@ -17,6 +17,15 @@ const Calendar = ({ newDataAdded }) => {
     "Dimanche",
   ];
 
+  if (!data) {
+    return (
+      <div>
+        Vous n'avez pas de rendez-vous. Ajoutez un rendez-vous pour visualiser
+        le calendrier.
+      </div>
+    );
+  }
+
   return (
     <div className="h-full flex items-center justify-center">
       <table className="calendar">
@@ -45,7 +54,7 @@ const Calendar = ({ newDataAdded }) => {
                 </div>
               </td>
               {daysOfWeek.map((day, index) => {
-                const appointment = data?.find(
+                const appointments = data.filter(
                   (appt) =>
                     appt.date === day &&
                     `${hour.toString().padStart(2, "0")}00` <= appt.startTime &&
@@ -53,42 +62,43 @@ const Calendar = ({ newDataAdded }) => {
                       `${(hour + 1).toString().padStart(2, "0")}00`
                 );
 
-                const slotHeight =
-                  Math.round(
-                    (appointment?.endTime - appointment?.startTime) / 50
-                  ) * 50;
-
                 return (
                   <td
                     key={index}
                     className="relative border border-gray-300 p-2 w-[10vw]"
                   >
-                    <div
-                      className={
-                        appointment
-                          ? "absolute inset-0 overflow-hidden w-[10vw] bg-sky-400 flex flex-col justify-center rounded-lg border border-gray-300"
-                          : ""
-                      }
-                      style={{
-                        height: `${(slotHeight * 6) / 100}vh`,
-                        marginTop:
-                          appointment?.startTime % 100 !== 0 ? "3vh" : "0",
-                      }}
-                    >
-                      {appointment && (
-                        <div className="text-[10px] text-white font-bold">
-                          <p>{appointment?.name}</p>
-                          <p className="text-slate-600">{`${appointment?.startTime.replace(
-                            /(\d{2})(\d{2})/,
-                            "$1h$2"
-                          )} à ${appointment?.endTime.replace(
-                            /(\d{2})(\d{2})/,
-                            "$1h$2"
-                          )}`}</p>
-                          <p>{appointment?.motif}</p>
+                    {appointments.map((appointment, appIndex) => {
+                      const slotHeight =
+                        Math.round(
+                          (appointment.endTime - appointment.startTime) / 50
+                        ) * 50;
+
+                      return (
+                        <div
+                          key={appIndex}
+                          className={
+                            "absolute inset-0 overflow-hidden w-[10vw] bg-sky-400 flex flex-col justify-center rounded-lg border border-gray-300"
+                          }
+                          style={{
+                            height: `${(slotHeight * 6) / 100}vh`,
+                            marginTop:
+                              appointment.startTime % 100 !== 0 ? "3vh" : "0",
+                          }}
+                        >
+                          <div className="text-[10px] text-white font-bold">
+                            <p>{appointment.name}</p>
+                            <p className="text-slate-600">{`${appointment.startTime.replace(
+                              /(\d{2})(\d{2})/,
+                              "$1h$2"
+                            )} à ${appointment.endTime.replace(
+                              /(\d{2})(\d{2})/,
+                              "$1h$2"
+                            )}`}</p>
+                            <p>{appointment.motif}</p>
+                          </div>
                         </div>
-                      )}
-                    </div>
+                      );
+                    })}
                   </td>
                 );
               })}
